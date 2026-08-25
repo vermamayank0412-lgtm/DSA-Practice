f90351f66678b7ffb1da436821e15961540c43e9
Mayank Verma <vermamayank0412@gmail.com>
2026-08-25 19:12:22 +0530

    Time: 2 ms (99.75%) | Memory: 43.3 MB (92.16%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+
+        StringBuilder ans = new StringBuilder();
+        int depth = 0;
+
+        for (char c : s.toCharArray()) {
+
+            if (c == '(') {
+                if (depth > 0) {
+                    ans.append(c);
+                }
+                depth++;
+            } 
+            else {
+                depth--;
+
+                if (depth > 0) {
+                    ans.append(c);
+                }
+            }
+        }
+
+        return ans.toString();
+    }
+}
